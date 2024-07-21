6c6d4ba3a7a110cd7845a7c3e93b005fd7502b85
Pavlo Kvas <kvaspasha@gmail.com>
2024-07-21 19:16:19 +0300

    save user on creation and use in other endpoints; The response returned from POST /api/users/:_id/exercises is the user object with the exercise fields added.

--- index.js
+++ index.js
@@ -3,26 +3,50 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 
+const users = {};
+
 app.use(cors());
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.post("/api/users", (req, res) => {
-  //todo
-  res.json({ username: "Pavlo", _id: "669d2288f3f0350013ad4330" });
+  const { username } = req.body;
+  const userId = Object.keys(users).length + 1;
+
+  // Check if the username already exists
+  const existingUser = Object.values(users).find(
+    (user) => user.username === username
+  );
+  if (existingUser) {
+    return res.json(existingUser);
+  }
+
+  const newUser = { username: username, _id: userId };
+  users[userId] = newUser;
+
+  res.json(newUser);
 });
 
 app.get("/api/users", (req, res) => {
-  //todo
-  res.json([{ username: "Pavlo", _id: "669d2288f3f0350013ad4330" }]);
+  res.json(Object.values(users));
 });
 
-app.post("/api/users/:_id/exercises", (req, res)=>{
-  //todo
-  res.json({});
-})
+app.post("/api/users/:_id/exercises", (req, res) => {
+  let date = req.body.date;
+  date = date ? new Date(date).toDateString() : new Date().toDateString();
+  res.json({
+    _id: req.params._id,
+    username: users[req.params._id].username, //todo
+    date: date,
+    duration: req.body.duration,
+    description: req.body.description,
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
